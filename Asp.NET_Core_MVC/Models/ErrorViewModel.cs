d7804246db816e9068c2e08188fe7e12ce9d91f2
Emre Polat <41800395+polatemre@users.noreply.github.com>
2021-12-07 19:07:06 +0300

    Delete Asp.NET_Core_MVC directory

--- Asp.NET_Core_MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Asp.NET_Core_MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
